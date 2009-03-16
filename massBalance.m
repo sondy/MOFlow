96e890b488c7a38e49bd6580f6f3ef96a27ca954
axs <axs@140d3012-c576-4164-9980-cb07aedb1c7e>
2009-03-16 21:08:35 +0000

    Started work on massBalance.m to determine the mass of the EDR and the EER.
    git-svn-id: svn+ssh://turin.mit.edu/home/axs/repos/matlab@19 140d3012-c576-4164-9980-cb07aedb1c7e

---
+++ massBalance.m
@@ -0,0 +1,10 @@
+%% massBalance.m
+% 3/16/2009; Alessondra Springmann
+% A routine to calculate what the relevant isotope concentrations should be
+% in the D" layer based on the mass of the D'' layer, the mass of the
+% mantle, and the concentrations of various elements in the mantle
+
+% massDprimeprime is the mass of the D'' layer
+
+mass_mantle = 
+
