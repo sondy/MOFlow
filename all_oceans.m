9ac43b87cc8b449344566b0ad8848253bdf59b4b
axs <axs@140d3012-c576-4164-9980-cb07aedb1c7e>
2009-06-10 00:59:10 +0000

    I created an AWESOME script that calls the ocean.m file for different depths and calls globals beforehand.  Probably not quite right, but it works.
    git-svn-id: svn+ssh://turin.mit.edu/home/axs/repos/matlab@136 140d3012-c576-4164-9980-cb07aedb1c7e

---
+++ all_oceans.m
@@ -0,0 +1,17 @@
+% calling all oceans...
+
+globals
+
+depths = linspace(1000, 3000, 5).*1000; 
+% linearly spaced magma ocean depths, all in meters
+
+CMB = 2885000;                  % *** m, radius of core-mantle boundary
+R = 6378000; 
+
+depths = cat(2, depths, R-CMB);
+
+num_oceans = [];
+
+for num_oceans = 1:size(depths, 2)
+    ocean(depths(num_oceans))
+end
--- globals.m
+++ globals.m
@@ -1,9 +1,13 @@
 % globals.m
 % June 7, 2009 Alessondra Springmann
+% a bunch of initializations to make running MOFlowEarth* be more pleasant
 
-CMB = 2885000;                  % *** m, radius of core-mantle boundary
-R = 6378000; 
+global Dolinv liquid maxstep r rinv solidinv % for later plotting/debugging
+global xrecord xrecordn % for determing ranges for fzero in BMsolid.m
+global num_oceans % for making the following into vectors
+global avgEER avgEERwLiq EERfracoftotal EERfracoftotalwliq avgEDR
 
-global Dolinv liquid maxstep r rinv solidinv xrecord xrecordn
+xrecordn = 0; % when you initialize a global, it gets cast as a vector.  
+% Incrementing a vector results in an empty vector.  Who knew.  Thanks
+% Henry.
 
-xrecordn = 0;
